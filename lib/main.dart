a68490012a6c748bab801f7572d85e62bfd01aae
Aimen Zeffanine <aimenzeffanine2024@gmail.com>
2024-11-28 11:31:35 +0100

    add screens and navigation and managing data using riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
+import 'package:favorite_places_app/screens/places.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
 import 'package:google_fonts/google_fonts.dart';
+
 
 final colorScheme = ColorScheme.fromSeed(
   brightness: Brightness.dark,
@@ -27,7 +28,7 @@ final theme = ThemeData().copyWith(
 
 void main() {
   runApp(
-      const ProviderScope(child:  MyApp(),),
+    const ProviderScope(child: MyApp()),
   );
 }
 
@@ -39,7 +40,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Great Places',
       theme: theme,
-      home: ...,
+      home: const PlacesScreen(),
     );
   }
 }
